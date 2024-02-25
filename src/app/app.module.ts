b9cf0caac45d8ec163b56d0a8a832fcba2c8ec8b
y-mane <keita.souleyman225@gmail.com>
2024-02-25 16:16:12 +0000

    adding login and register forms and controls rules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { CardModule } from 'primeng/card';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import {ReactiveFormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    InputTextareaModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm = this.fb.group({
+    firstName: ['', [Validators.required,Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z])*$/)]],
+    lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z])*$/)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  });
 
+    constructor(private fb:FormBuilder) {
+    }
+
+    get firstName(){
+    return this.registerForm.controls['firstName'];
+    }
+  get lastName(){
+    return this.registerForm.controls['lastName'];
+  }
+  get email(){
+    return this.registerForm.controls['email'];
+  }
+  get password(){
+    return this.registerForm.controls['password'];
+  }
+  get confirmPassword(){
+    return this.registerForm.controls['confirmPassword'];
+  }
 }
